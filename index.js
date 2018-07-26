f9c7d5f7952fc33ea6e064fdd1f8c8815661cd3c
Elizzabeta <39170278+Elizzabeta@users.noreply.github.com>
2018-07-26 21:33:10 +0200

    cas1

---
+++ index.js
@@ -0,0 +1,68 @@
+/*var integer1 = 5;
+var integer2 = 10;
+
+var sum = integer1+integer2;
+
+console.log(integer1 + " + " + intereg2 + " = " + sum);
+*/
+/*
+var name = "Elizabeta";
+var pass = "0000000";
+var nameUpdated = name.toLowerCase();
+console.log(name.toLowerCase());
+
+if(nameUpdated == "elizabeta"){
+	console.log("Hello " + name);
+}
+else{
+	console.log("Someone else");
+}
+/**/
+/*if( name == "Elizabeta" || name= "elizabeta" || name == "ELIZABETA"){
+	console.log("Hello " + name);
+}
+else{
+	console.log("Someone else");
+}
+
+switch(name){
+	case "Elizabeta":
+	console.log("Hello " + name);
+	break;
+	case "Elizabeta":
+	console.log("Hello " + name);
+	break;
+	default:
+	console.log("Unsucessful");
+}*/
+
+//**********************************************************
+/*
+var firstName = "Elizabeta";
+var lastName = "Jovanovska";
+
+var fullName = firstName.concat(lastName);
+console.log(fullName);*/
+//*************************************************************
+
+var integer = [3,4,5,6,7,8,9,10];
+
+integer[100] = 50;
+
+console.log(integer);
+
+/*for(let i = 0; i < integer.length; i++){
+	console.log(integer[i] +": ");
+
+	if(integer[i]%3 == 0){
+		console.log("buzz");
+	}
+
+	else if(integer[i] % 4 == 0){
+		console.log("fiz");
+	}
+
+	console.log("\n");
+}*/
+
+
